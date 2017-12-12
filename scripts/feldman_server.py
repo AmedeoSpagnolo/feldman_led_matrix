db9c0b2f1aff7a02c80aae1df5ed66c915d7ef2a
Amedeo SPAGNOLO <amedeo.spagnolo@gmail.com>
2017-12-12 17:02:35 +0800

    add server feldman api communication

--- scripts/feldman_server.py
+++ scripts/feldman_server.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python
 import requests
-r = requests.get('http://localhost:8080')
-print r.content
+import threading
+
+blacklist = ["Jacob", "William", "Ethan", "James", "Alexander", "Jackson", "David", "Oliver", "Jayden", "Joseph", "Gabriel", "Samuel", "Carter", "Anthony", "John", "Dylan", "Luke", "Henry", "Andrew", "Isaac", "Christopher", "Joshua", "Wyatt", "Sebastian", "Owen", "Caleb", "Nathan"]
+
+def print_on_led_matrix(word):
+    print word
+
+def blacklisted(word):
+    if (word in blacklist):
+        return False
+    else:
+        return True
+
+def request():
+    r = requests.get('http://localhost:8080')
+    word = r.content.strip('"')
+    if (r.status_code == 200):
+        if (blacklisted(word)):
+            # time.sleep(2.0)
+            threading.Timer(2.0, request).start()
+            print_on_led_matrix(word)
+        else:
+            print "blacklisted: %s" % word
+            request()
+    else:
+        print "not found!!"
+
+request()
