6cd0035a82c7850ffd79ebaa500f4939cf6147bc
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2017-12-12 20:55:10 +0800

    refactoring and claening

--- scripts/feldman_server1.py
+++ scripts/feldman_server1.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python
-# from samplebase import SampleBase
-# from rgbmatrix import graphics
+from samplebase import SampleBase
+from rgbmatrix import graphics
 import time
 import json
 import requests
 import threading
 import random
 
-rbgmatrix = {}
-SampleBase = {}
+prev_word = ""
 
-previous_word = ""
-
-# class Feld(SampleBase):
-class Feld():
+class Feld(SampleBase):
     def __init__(self, *args, **kwargs):
-        # super(Feld, self).__init__(*args, **kwargs)
-        pass
+        super(Feld, self).__init__(*args, **kwargs)
+        canvas      = self.matrix.CreateFrameCanvas()
+        blacklist   = json.load(open('blacklist.json'))
+        feldloop    = json.load(open('feldloop.json'))
+        font        = graphics.Font()
+        anim_time   = 100
 
-    def print_on_led_matrix(word, prev_word):
+    def print_word(word):
 
-        margin_bottom  = 2
-        margin_top = self.matrix.height - 8
-        word_length = margin_bottom + ll("FELD" + previous_word)
-        prev_word_length = margin_bottom + ll("FELD" + word)
-        sign = 1 if (_from < _to) else -1
+        def ll(string):
+            return sum([font.CharacterWidth(ord(c)) for c in string])
 
-        while c < steps:
+        count   = 0
+        font    = self.font
+        canvas  = self.convas
+
+        margin_bottom   = 2
+        margin_top      = self.matrix.height - 8
+        word_length     = margin_bottom + ll("FELD" + word)
+        prev_word_len   = margin_bottom + ll("FELD" + prev_word)
+        sign = 1 if (prev_word_len < word_length) else -1
+
+        font.LoadFont("../fonts/4x6.bdf")
+
+        while c < anim_time:
+            canvas.Clear()
 
             # line
             line = {}
@@ -58,54 +68,37 @@ class Feld():
             man.color = graphics.Color(255,255,255)
             graphics.DrawText(canvas, font, man.x0, man.y0, man.color, word)
 
-    def blacklisted(self, word):
-        return False if (word in blacklist) else True
+            c += 1
+            time.sleep(0.01)
 
-    def request(self):
-        r = requests.get('http://localhost:8080')
-        word = r.content.strip('"')
-        if (r.status_code == 200):
-            if (self.blacklisted(word) and word != "empty"):
-                # time.sleep(2.0)
-                threading.Timer(2.0, request).start()
-                print_on_led_matrix(word)
-            elif(word == "empty"):
-                threading.Timer(1.0, request).start()
-                print "-"
-                print_on_led_matrix(feldloop[random.randint(0,len(feldloop)-1)])
+            canvas = self.matrix.SwapOnVSync(canvas)
+
+        prev_word = word
+
+    def isblacklisted(self,word):
+        return True if (word in self.blacklist) else False
+
+    def get_word_from_list():
+        return self.feldloop[random.randint(0,len(self.feldloop)-1)]
+
+    def get_word_from_api(self, url):
+        try:
+            r = requests.get(url)
+            word = r.content[1:-1]
+            if (self.isblacklisted(word)):
+                return word
             else:
                 print "blacklisted: %s" % word
-                request()
-        else:
-            print "not found!!"
+                return False
+        except requests.exceptions.RequestException as e:
+            print e
+            return False
 
     def run(self):
-        print "run"
-        # canvas = self.matrix.CreateFrameCanvas()
-        #
-        # # assets
-        # font        = graphics.Font()
-        # blacklist   = json.load(open('blacklist.json'))
-        # feldloop    = json.load(open('feldloop.json'))
-        # font.LoadFont("../fonts/4x6.bdf")
-        # max_brightness = self.matrix.brightness
-        #
-        # def ll(string):
-        #     return sum([font.CharacterWidth(ord(c)) for c in string])
-        #
-        # count = 0
-        # word = request()
-        # print_on_led_matrix(word)
-        #     c += 1
-        #     time.sleep(0.01)
-        #     canvas = self.matrix.SwapOnVSync(canvas)
-        #     canvas.Clear()
-        #
-        # time.sleep(1.5)
-        #
-        # count += 1
-        # count = count % len(loop)
-
+        temp = self.get_word_from_api("http://localhost:8080")
+        word = temp if (temp and temp != None) else self.get_word_from_list()
+        print_word(word)
+        threading.Timer(2.0, run).start()
 
 # Main function
 if __name__ == "__main__":
