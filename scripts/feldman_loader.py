736b05870889a9841c183454e1ad6fe908edbf97
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2017-12-16 16:16:55 +0800

    add args in feldman lib

--- scripts/feldman_loader.py
+++ scripts/feldman_loader.py
@@ -3,10 +3,34 @@ from samplebase import SampleBase
 from rgbmatrix import graphics
 import time
 import json
+import argparse
 
-class Feld(SampleBase):
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
+    pass
+
+parser = argparse.ArgumentParser(
+    add_help=False,
+    formatter_class=CustomFormatter)
+parser.add_argument(
+    '-v',
+    '--version',
+    action='version',
+    version='%(prog)s 1.0')
+parser.add_argument(
+    '-s',
+    '--simple_loader',
+    action='store_true')
+parser.add_argument(
+    '-c',
+    '--call_api',
+    action='store_true')
+
+args = parser.parse_args()
+
+class Feld(SampleBase, obj = {}):
     def __init__(self, *args, **kwargs):
         super(Feld, self).__init__(*args, **kwargs)
+        print obj
 
     def run(self):
         canvas = self.matrix.CreateFrameCanvas()
@@ -62,7 +86,10 @@ class Feld(SampleBase):
 
 # Main function
 if __name__ == "__main__":
-    feldman = Feld()
+    if args.simple_loader:
+        feldman = Feld({"api": False})
+    elif args.call_api:
+        feldman = Feld({"api": True})
     if (not feldman.process()):
         feldman.print_help()
 
