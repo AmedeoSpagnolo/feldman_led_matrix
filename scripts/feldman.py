368a2fb9cda3a3969e3e4d1f3998d06cafe0a11d
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2017-12-16 17:40:29 +0800

    minor fix

--- scripts/feldman.py
+++ scripts/feldman.py
@@ -4,6 +4,9 @@ import argparse
 sys.path.append(sys.path[0])
 from libs.liboris import *
 
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
+    pass
+
 parser = argparse.ArgumentParser(
     add_help=True,
     description="Boris Feldman Script by Amedeo Spagnolo",
@@ -21,6 +24,8 @@ parser.add_argument(
     action='store_true')
 
 args = parser.parse_args()
+
+print args
 
 if __name__ == "__main__":
     feldman = Feld({"api": False})
