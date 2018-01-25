2357f3b624bd323b8139abf6c045a283c3c7f8e1
Amedeo SPAGNOLO <amedeo.spagnolo@gmail.com>
2018-01-25 17:43:57 +0800

    socket works with problem

--- test/python_socket_io.py
+++ test/python_socket_io.py
@@ -6,28 +6,29 @@ from flask import Flask, render_template
 sio = socketio.Server()
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    """Serve the client-side application."""
-    return "sdf"
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     """Serve the client-side application."""
+#     return "sdf"
+#     return render_template('index.html')
+#
+# @sio.on('connect', namespace='/chat')
+# def connect(sid, environ):
+#     print("connect ", sid)
 
-@sio.on('connect', namespace='/chat')
-def connect(sid, environ):
-    print("connect ", sid)
-
-@sio.on('chat message', namespace='/chat')
+@sio.on('news')
 def message(sid, data):
-    print("message ", data)
-    sio.emit('reply', room=sid)
+    print data
+    # print("message ", data)
+    # sio.emit('reply', room=sid)
 
-@sio.on('disconnect', namespace='/chat')
-def disconnect(sid):
-    print('disconnect ', sid)
+# @sio.on('disconnect', namespace='/chat')
+# def disconnect(sid):
+#     print('disconnect ', sid)
 
 if __name__ == '__main__':
     # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
 
     # deploy as an eventlet WSGI server
-    eventlet.wsgi.server(eventlet.listen(('', 12346)), app)
+    eventlet.wsgi.server(eventlet.listen(('', 12345)), app)
