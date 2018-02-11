e44092b6247f7b7c4a341e8f799ad80a7f2c4bd9
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2018-02-11 20:27:37 +0800

    test new font

--- scripts/libs/config.py
+++ scripts/libs/config.py
@@ -4,7 +4,11 @@ FONT = graphics.Font()
 MAIN_COLOR = graphics.Color(255,255,255)
 
 MARGIN_LEFT = 1
-MARGIN_TOP = 11
+MARGIN_TOP = 16
+
+print dir(FONT)
+print FONT.height
+print FONT.baseline
 
 FELD_LOOP = [
   "man",
@@ -21,9 +25,9 @@ FELD_LOOP = [
 ]
 
 # FONT.LoadFont("libs/fonts/4x6.bdf")
-FONT.LoadFont("libs/fonts/converted.bdf")
+FONT.LoadFont("libs/fonts/converted_5.bdf")
 
 ANIMATION_TIME = 10
 SHIFT = 5
 
-PREFIX = "FELD"
+PREFIX = "feld"
